f9d49e062a86c74385fe8cee15b61d58480a59ac
Andrew Cho <cisdell@gmail.com>
2023-03-11 16:26:27 -0500

    added more routes

--- main.py
+++ main.py
@@ -1,21 +1,46 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
+from random import randrange
 
 app = FastAPI()
 
 
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+
+my_posts = [{"title": "title of post1", "content": "content of post 1",
+             "id": 1}, {"title": "favorite foods", "id": 2}]
+
+def find_post(id):
+    for p in my_posts:
+        if p["id"] == id:
+            return p
+
 @app.get("/")
 def root():
-    return {"message": "test test"}  # automatically converts to JSON.
+
+    return {"data": "my first data"}
 
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "This is your posts"}
+    return {"data": my_posts}
 
 
-@app.post("/createposts")
-def create_posts(payLoad: dict = Body(...)):
-    print(payLoad)
-    # return {"message":"successfully created posts"}
-    return {"new_post": f"title {payLoad['title']} content: {payLoad['content']}"}
+@app.post("/posts")
+def create_posts(post: Post):
+    # print(post.dict()) #turns into a standard python dictionary.
+    post_dict = post.dict()
+    my_posts.append(post_dict)
+    return {"data": post}
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    post = find_post(id)
+    return {"post_detail": post}
