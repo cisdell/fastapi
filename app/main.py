efe23e596742c047aeb74618f0332fd23b94cff9
Andrew Cho <cisdell@gmail.com>
2023-05-14 19:05:47 -0400

    using env file

--- app/main.py
+++ app/main.py
@@ -4,12 +4,9 @@ from sqlalchemy.orm import Session
 from . import models, schemas
 from .database import engine
 from .routers import post, user, auth
-
+from .config import settings
 models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
-
-# data1 = cursor.execute('SELECT * FROM posts;')
-# print(data1)
 
 app.include_router(post.router)
 app.include_router(user.router)
