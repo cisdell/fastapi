b76a1e2a811f352bea14a14a7c9a997b5dc0892f
Andrew Cho <cisdell@gmail.com>
2023-05-15 11:40:42 -0400

    vote count endpoint

--- app/routers/post.py
+++ app/routers/post.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
 from typing import List, Optional
 from sqlalchemy.orm import Session
+from sqlalchemy import func
 from .. import models, schemas, oauth2
 from ..database import get_db
 
@@ -9,12 +10,14 @@ router = APIRouter(
     tags=['Posts']
 )
 
-
-@router.get("/", status_code=status.HTTP_200_OK, response_model=List[schemas.Post])
+#@router.get("/", status_code=status.HTTP_200_OK, response_model=List[schemas.Post])
+@router.get("/")
 def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user), limit: int = 10, skip: int = 0, search: Optional[str] =""):
     posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
     #print('this just ran:::', limit)
-    return posts
+    results = db.query(models.Post, func.count(models.Vote.post_id).label("vote")).join(models.Vote, models.Vote.post_id == models.Post.id, isouter=True).group_by(models.Post.id).all()
+    #print(results)
+    return results
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
