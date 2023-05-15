015ef06b00a0f267002196614cd01fa9021b9504
Andrew Cho <cisdell@gmail.com>
2023-05-14 23:41:30 -0400

    fixed voting bug

--- app/routers/vote.py
+++ app/routers/vote.py
@@ -10,9 +10,10 @@ router = APIRouter(
 @router.post("/", status_code=status.HTTP_201_CREATED)
 def vote(vote: schemas.Vote, db: Session = Depends(database.get_db), current_user: int = Depends(oauth2.get_current_user)):
     vote_query = db.query(models.Vote).filter(models.Vote.post_id == vote.post_id, models.Vote.user_id == current_user.id)
-
     found_vote = vote_query.first()
-    if (vote.dir == 1):
+    if found_vote == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f" The vote {vote.post_id} does not exist")
+    elif (vote.dir == 1):
         if found_vote:
             raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"user {current_user.id} has already voted on post {vote.post_id}")
         new_vote = models.Vote(post_id = vote.post_id, user_id=current_user.id)
