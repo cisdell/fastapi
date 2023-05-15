ff79a9a50ff4cc7f0e2c1f1548b0efdbdae4cd32
Andrew Cho <cisdell@gmail.com>
2023-05-15 12:00:43 -0400

    fixed bugs

--- app/routers/post.py
+++ app/routers/post.py
@@ -11,13 +11,15 @@ router = APIRouter(
 )
 
 #@router.get("/", status_code=status.HTTP_200_OK, response_model=List[schemas.Post])
-@router.get("/")
+@router.get("/", status_code=status.HTTP_200_OK)
 def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user), limit: int = 10, skip: int = 0, search: Optional[str] =""):
-    posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
+    # posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
     #print('this just ran:::', limit)
-    results = db.query(models.Post, func.count(models.Vote.post_id).label("vote")).join(models.Vote, models.Vote.post_id == models.Post.id, isouter=True).group_by(models.Post.id).all()
-    #print(results)
-    return results
+
+    posts = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(
+        models.Vote, models.Vote.post_id == models.Post.id, isouter=True).group_by(models.Post.id).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
+    return posts
+    #return results
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
